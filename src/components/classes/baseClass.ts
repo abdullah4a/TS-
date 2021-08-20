2dd67a83fc82e1a81829bd7fae2e6e2fb82ec071
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-20 23:22:58 +0500

    Update baseClass.ts

--- src/components/classes/baseClass.ts
+++ src/components/classes/baseClass.ts
@@ -4,6 +4,7 @@ export default class baseClass extends Vue {
   private msg = "Hello to Vue Ts";
   functionName() {
     let name = prompt("Enter Name:");
-    alert(name);
+    // alert(name);
+    console.info(name);
   }
 }
