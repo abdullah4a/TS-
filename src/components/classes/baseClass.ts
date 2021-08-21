58149f2b0fa0e5b0bb1f6607a2f6ccab161b7f03
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-21 11:13:39 +0500

    Succeeded Alhamdulillah

--- src/components/classes/baseClass.ts
+++ src/components/classes/baseClass.ts
@@ -2,9 +2,10 @@ import { Vue } from "vue-property-decorator";
 
 export default class baseClass extends Vue {
   private msg = "Hello to Vue Ts";
-  functionName() {
-    let name = prompt("Enter Name:");
+  private Name = "";
+  functionName(): string {
+    this.Name = prompt("Enter Name:");
     // alert(name);
-    console.info(name);
+    return this.Name;
   }
 }
--- src/components/comp.vue
+++ src/components/comp.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button class="btn" @click="NameFunction">Show</button>
+    <h1 v-if="firstName">Assalam o Alaikum {{ firstName }}</h1>
+    <h2 v-if="lastName">
+      Mr. {{ lastName }} <br />
+      we're Happy to see you here
+    </h2>
+    <label for="btn">Click on 'Enter Name'</label><br />
+    <button class="btn" @click="NameFunction">Enter Name</button>
+    <div v-if="NamesArray">
+      <h2>Entered Names are in Here :</h2>
+      <h5 v-for="arr in NamesArray" :key="arr">{{ arr }}</h5>
+    </div>
   </div>
 </template>
 
@@ -11,8 +20,21 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import baseClass from "./classes/baseClass";
 @Component
 export default class HelloWorld extends baseClass {
+  private superName = "";
+  private firstName = "";
+  private lastName = "";
+  private NamesArray = [""];
   NameFunction() {
-    this.functionName();
+    this.superName = this.functionName();
+    this.NamesArray.push(this.superName);
+    this.firstName = this.superName
+      ?.split(" ")
+      .slice(0, -1)
+      .join(" ");
+    this.lastName = this.superName
+      ?.split(" ")
+      .splice(-1)
+      .join(" ");
   }
 }
 </script>
