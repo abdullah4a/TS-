ce9b1316857faafea4547d422a016123842db7b7
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-21 11:18:11 +0500

    Update Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,8 @@
+<template> </template>
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+export default class Login extends Vue {
+  private Uname = "";
+  private password = "";
+}
+</script>
