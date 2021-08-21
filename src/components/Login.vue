1aab211fbd311b5a236671f53b16a6fd77e98976
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-21 13:15:22 +0500

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,13 @@
-<template> </template>
+<template>
+  <div>
+    <form action="post" @submit="login">
+      <label for="UName">Enter UserName</label>
+      <input type="text" id="UName " />
+      <label for="Password">Enter Password</label>
+      <input type="password" id="Password" />
+      <input type="submit" value="Login" />
+    </form></div
+></template>
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
 export default class Login extends Vue {
